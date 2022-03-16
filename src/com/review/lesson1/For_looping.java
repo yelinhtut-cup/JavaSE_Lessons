b9e6fd495bcab53dbfe68f37aaf553e56003e106
yelinhtut-cup <ylin8617@gmail.com>
2022-03-16 12:27:59 +0630

    part one

--- src/com/review/lesson1/For_looping.java
+++ src/com/review/lesson1/For_looping.java
@@ -1,2 +1,15 @@
-package com.review.lesson1;public class For_looping {
+package com.review.lesson1;
+
+import java.util.Scanner;
+
+public class For_looping {
+    public static void main(String[] args) {
+
+        for ( int i=1;i<=5;i++){
+            for ( int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
 }
